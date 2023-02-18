99bfb6026c83305ac89c2828dcf10e5d077f4ae2
artT14 <lamferme@gmail.com>
2023-02-17 21:46:41 -0800

    Able to render data, time to make it pretty, and add more functionality, maybe even refactor

--- forceGraph.js
+++ forceGraph.js
@@ -4,44 +4,83 @@ document.addEventListener('DOMContentLoaded', function () {
 		const message = event.data; // The JSON data our extension sent
 		switch (message.command) {
 			case 'lsJsonData':
-				dataVariable = message.data;
-				console.log(dataVariable)
-				drawTree()
+				const graphData = parseNpmGraph(message.data)
+				drawTree(graphData)
 				break;
 		}
 	})
 }) 
+function parseNpmGraph (string){
+	const graphData = {
+		nodes: [],
+		links: []
+	} 
+	// run dfs
+	const hash = new Set();
+	const tree = JSON.parse(string)
 
+	function dfs(node,data, layer){
+		if (!data.dependencies) return;
+		Object.entries(data.dependencies)
+			.filter(([key, val])=>!hash.has(key+val.version))
+			.forEach(([key, val])=>{
+				graphData.nodes.push({
+					id: key+val.version,
+					name: key+val.version,
+					layer
+				});
+				graphData.links.push({
+					source: key+val.version,
+					target: node+data.version
+				})
+				hash.add(key+val.version);
+				dfs(key,val,layer+1);
+			})
+	}
+	// add main node
+	graphData.nodes.push({
+		id: tree.name+tree.version,
+		name: tree.name+tree.version,
+		layer: 0
+	})
+	hash.add(tree.name+tree.version)
 
-function drawTree(){
+	// add first layer of dependencies
+	Object.entries(tree.dependencies).forEach(([key, val])=>{
+		graphData.nodes.push({
+			id: key+val.version,
+			name: key+val.version,
+			layer: 1
+		});
+		graphData.links.push({
+			source: key+val.version,
+			target: tree.name+tree.version
+		})
+		hash.add(key+val.version);
+		dfs(key,val, 2);
+	})
+	return graphData;
+}
+
+function drawTree(graphData){
+	console.log(graphData)
 	const N = 20;
-	const gData = {
-		nodes: [...Array(N).keys()].map(i => ({ id: i })),
-		links: [...Array(N).keys()]
-		.filter(id => id)
-		.map(id => ({
-			source: id,
-			target: Math.round(Math.random() * (id-1))
-		}))
-	};
+	// const gData = {
+	// 	nodes: [...Array(N).keys()].map(i => ({ id: i })),
+	// 	links: [...Array(N).keys()]
+	// 	.filter(id => id)
+	// 	.map(id => ({
+	// 		source: id,
+	// 		target: Math.round(Math.random() * (id-1))
+	// 	}))
+	// };
+	// console.log(gData);
 
 	// gen a number persistent color from around the palette
-	const getColor = n => '#' + ((n * 1234567) % Math.pow(2, 24)).toString(16).padStart(6, '0');
 
 	const Graph = ForceGraph()
 		(document.getElementById('graph'))
-		.nodeCanvasObject((node, ctx) => nodePaint(node, getColor(node.id), ctx))
-		.nodePointerAreaPaint(nodePaint)
-		.nodeLabel('id')
-		.graphData(gData);
-
-	function nodePaint({ id, x, y }, color, ctx) {
-		ctx.fillStyle = color;
-		[
-		() => { ctx.fillRect(x - 6, y - 4, 12, 8); }, // rectangle
-		() => { ctx.beginPath(); ctx.moveTo(x, y - 5); ctx.lineTo(x - 5, y + 5); ctx.lineTo(x + 5, y + 5); ctx.fill(); }, // triangle
-		() => { ctx.beginPath(); ctx.arc(x, y, 5, 0, 2 * Math.PI, false); ctx.fill(); }, // circle
-		() => { ctx.font = '10px Sans-Serif'; ctx.textAlign = 'center'; ctx.textBaseline = 'middle'; ctx.fillText('Text', x, y); } // text
-		][id%4]();
-	}
+		.graphData(graphData)
+		.nodeAutoColorBy('layer')
+		;
 }
